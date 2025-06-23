46bfea9982d210eea55463fc3068cbad8da2cb9c
colebrookson <cole.brookson@gmail.com>
2025-06-23 15:55:21 -0400

    change the targets local vs hpc settings

--- _targets.R
+++ _targets.R
@@ -4,23 +4,28 @@ source("./packages.R")
 # load environment variables
 dotenv::load_dot_env()
 
+# PARALLEL SETTINGS: local vs HPC
+run_env <- Sys.getenv("TAR_RUN_ENV", unset = "local")
+if (run_env == "hpc") {
+  targets::tar_option_set(
+    workspace_on_error = TRUE
+  )
+}
+
 # Set target options with targets::tar_option_set() - useful for tuning performance and setting up pipelines on HPCs
 
-## Load functions stored in /R 
+## Load functions stored in /R
 targets::tar_source()
 
 ## logical collection of targets that does some task
 example_tar_plan <- tar_plan(
-  
-  # tar_target(targets_are_nouns, functions_are_verbs(arg)) 
-  
+
+  # tar_target(targets_are_nouns, functions_are_verbs(arg))
 )
 
 example_tar_plan2 <- tar_plan(
-  
-   tar_target(data, get_data(arg)),
-   tar_target(data_cleaned, clean_data(data))
-  
+  tar_target(data, get_data(arg)),
+  tar_target(data_cleaned, clean_data(data))
 )
 
 # add tar_plan outputs to this list to have targets make them.
